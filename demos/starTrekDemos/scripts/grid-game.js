f33493e3e7b31527f251a39a7190c85b2f14f310
TJ Davis <thomas.davis@mavs.uta.edu>
2022-05-16 16:47:24 +0800

    More game logic, updated UI
    Updated board to look more like a computer. Made some game logic - still need to verify calculations and stuff.

--- demos/starTrekDemos/scripts/grid-game.js
+++ demos/starTrekDemos/scripts/grid-game.js
@@ -37,6 +37,30 @@ function assembleGame(){
     r = Math.sqrt((dx)^2+(dy)^2)
     theta_RADIANS = Math.atan2(dy,dx);
     theta_DEGREES = Math.atan2(dy,dx)* 180/Math.PI;
+
+    var elt = document.getElementById('calculator');
+    var calculator = Desmos.GraphingCalculator(elt,{
+        expressions:false,lockViewport:true
+    });
+    calculator.setMathBounds({
+        left:0.75,
+        right:5.25,
+        bottom:0.75,
+        top:5.25
+    });
+    //graphs a right triangle corresponding to the two points of the 
+    //klingons, the enterprise, and a point that makes it a right triangles 
+    calculator.setExpression({ 
+        id: 'points', 
+        latex: `[(${K['kx']},${K['ky']}),(${K['kx']},${E['ey']}),(${E['ex']},${E['ey']}),(${K['kx']},${K['ky']})]`,
+        lines:true
+    
+    });
+    
+
+    toggleButton = document.querySelector("input#showGraph");
+    toggleButton.style.visibility="visible";
+    alert("Spock says: There is a klingon vessel nearby. I advise you to strike before they do. ")
 }
 
 function klingonWarbird(){
@@ -60,4 +84,18 @@ function enterprise(x,y){
         "ex": e_x,
         "ey": e_y    
     }
+}
+
+function toggleVisibility(){
+    var elt = document.getElementById('calculator');
+    toggleButton = document.querySelector("input#showGraph");
+    if( elt.style.visibility =='visible'){
+        elt.style.visibility= '';
+        toggleButton.value='Show graph'
+    } else{
+        elt.style.visibility='visible';
+        toggleButton.value='Hide graph'
+    }
+    
+    
 }
